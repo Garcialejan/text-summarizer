6ada72f2ad2fc784599066fa04a015a57d2acb97
Alejandro_pc\Alejandro <garcialejan@gmail.com>
2025-11-15 17:58:43 +0100

    Logging and common functionalities added

--- template_generator.py
+++ template_generator.py
@@ -6,30 +6,26 @@ project_name = "text_summarizer"
 logging.basicConfig(level = logging.INFO,
                     format  ='[%(asctime)s]: %(message)s:')
 
-list_of_files = [
+list_of_files=[
     ".github/workflows/.gitkeep",
-    f"src/{project_name}/__init__.py", 
-    f"src/{project_name}/components/__init__.py", 
-    f"src/{project_name}/utils/__init__.py", 
-    f"src/{project_name}/utils/common.py", 
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
-    f"src/{project_name}/exception/__init__.py", 
-    f"src/{project_name}/entity/__init__.py", 
-    f"src/{project_name}/entity/config_entity.py", 
+    f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
-    f"src/{project_name}/cloud/__init__.py",
-    "research/research.ipynb",
     "config/config.yaml",
-    "Network_Data/",
+    "params.yaml",
     "app.py",
     "main.py",
     "Dockerfile",
-    # "requierements.txt",
-    # "setup.py", #! Use to create the entire project as a package
-    # ".env"
+    "requirements.txt",
+    "setup.py",
+    "research/research.ipynb"
 ]
 
 # Loop to create all fthe dirs and files above
