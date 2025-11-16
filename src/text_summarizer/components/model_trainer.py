908954c289fbe44f31ab57a586bad7ee23eedfba
Alejandro G. WSL <garcialejan@gmail.com>
2025-11-16 19:39:28 +0100

    Problems solved with model trainer params

--- src/text_summarizer/components/model_trainer.py
+++ src/text_summarizer/components/model_trainer.py
@@ -16,23 +16,23 @@ class ModelTrainer:
         device = "cuda" if torch.cuda.is_available() else "cpu"
         tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
         model_bart = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
-        seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_pegasus)
+        seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_bart)
 
         #loading the data
         dataset_samsum_pt = load_from_disk(self.config.data_path)
 
         trainer_args = TrainingArguments(
             output_dir = self.config.root_dir,
-            num_train_epochs = self.config.num_train_epochs,
-            warmup_steps = self.config.warmup_steps,
-            per_device_train_batch_size = self.config.per_device_train_batch_size,
-            per_device_eval_batch_size = self.config.per_device_eval_batch_size,
-            weight_decay = self.config.weight_decay, 
-            logging_steps = self.config.logging_steps,
+            num_train_epochs = int(self.config.num_train_epochs),
+            warmup_steps = int(self.config.warmup_steps),
+            per_device_train_batch_size = int(self.config.per_device_train_batch_size),
+            per_device_eval_batch_size = int(self.config.per_device_eval_batch_size),
+            weight_decay = float(self.config.weight_decay), 
+            logging_steps = int(self.config.logging_steps),
             eval_strategy = self.config.evaluation_strategy,
-            eval_steps = self.config.eval_steps,
-            save_steps = self.config.save_steps,
-            gradient_accumulation_steps = self.config.gradient_accumulation_steps
+            eval_steps = int(self.config.eval_steps),
+            save_steps = float(self.config.save_steps),
+            gradient_accumulation_steps = int(self.config.gradient_accumulation_steps)
         )
         
         trainer = Trainer(model = model_bart,
--- src/text_summarizer/config/configuration.py
+++ src/text_summarizer/config/configuration.py
@@ -50,16 +50,16 @@ class ConfigurationManager:
             root_dir = config.root_dir,
             data_path = config.data_path,
             model_ckpt = config.model_ckpt,
-            num_train_epochs = params.num_train_epochs,
-            warmup_steps = params.warmup_steps,
-            per_device_train_batch_size = params.per_device_train_batch_size,
-            per_device_eval_batch_size = params.per_device_eval_batch_size,
-            weight_decay = params.weight_decay,
-            logging_steps = params.logging_steps,
+            num_train_epochs = int(params.num_train_epochs),
+            warmup_steps = int(params.warmup_steps),
+            per_device_train_batch_size = int(params.per_device_train_batch_size),
+            per_device_eval_batch_size = int(params.per_device_eval_batch_size),
+            weight_decay = float(params.weight_decay),
+            logging_steps = int(params.logging_steps),
             evaluation_strategy = params.evaluation_strategy,
-            eval_steps = params.evaluation_strategy,
-            save_steps = params.save_steps,
-            gradient_accumulation_steps = params.gradient_accumulation_steps
+            eval_steps = int(params.eval_steps),
+            save_steps = float(params.save_steps),
+            gradient_accumulation_steps = int(params.gradient_accumulation_steps)
         )
         
         return model_trainer_config
