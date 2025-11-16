587147a3703d5293280f99d0aa026804b3b6fddb
Alejandro_pc\Alejandro <garcialejan@gmail.com>
2025-11-16 19:51:45 +0100

    Model evaluation pipeline implemented

--- src/text_summarizer/config/configuration.py
+++ src/text_summarizer/config/configuration.py
@@ -3,7 +3,8 @@ from src.text_summarizer.utils.common import read_yaml, create_directories
 
 from src.text_summarizer.entity.config_entity import DataIngestionConfig, \
                                                         DataTransformationConfig, \
-                                                        ModelTrainerConfig
+                                                        ModelTrainerConfig, \
+                                                        ModelEvaluationConfig
 
 class ConfigurationManager:
     def __init__(self,
@@ -27,6 +28,7 @@ class ConfigurationManager:
         
         return data_ingestion_config
     
+    
     def get_data_transformation_config(self)-> DataTransformationConfig:
         config = self.config.data_transformation
 
@@ -39,6 +41,7 @@ class ConfigurationManager:
         )
         
         return data_transformation_config
+    
     
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         config = self.config.model_trainer
@@ -63,3 +66,19 @@ class ConfigurationManager:
         )
         
         return model_trainer_config
+    
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir = config.root_dir,
+            data_path = config.data_path,
+            model_path = config.model_path,
+            tokenizer_path = config.tokenizer_path,
+            metric_file_name = config.metric_file_name  
+        )
+
+        return model_evaluation_config
--- test_pipelines.py
+++ test_pipelines.py
@@ -5,6 +5,8 @@ from src.text_summarizer.exception.exception import TextSummarizerException
 from src.text_summarizer.pipeline.stage1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.text_summarizer.pipeline.stage2_data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.text_summarizer.pipeline.stage3_model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.text_summarizer.pipeline.stage4_model_evaluation_pipeline import ModelEvaluationTrainingPipeline
+
 
 STAGE_NAME="Data Ingestion stage"
 try:
@@ -32,5 +34,16 @@ try:
     model_trainer_pipeline = ModelTrainerTrainingPipeline()
     model_trainer_pipeline.initiate_model_trainer()
     logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    raise TextSummarizerException(e, sys)
+
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     raise TextSummarizerException(e, sys)
