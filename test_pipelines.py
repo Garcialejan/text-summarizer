d6e8fc573b5901f992e3bddd0e9b8678deb62aaa
Alejandro_pc\Alejandro <garcialejan@gmail.com>
2025-11-16 18:27:18 +0100

    Configuration and data ingestion implemented

---
+++ test_pipelines.py
@@ -0,0 +1,13 @@
+import sys
+from src.text_summarizer.logging import logger
+from src.text_summarizer.pipeline.stage1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.text_summarizer.exception.exception import TextSummarizerException
+
+STAGE_NAME="Data Ingestion stage"
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    raise TextSummarizerException(e, sys)
